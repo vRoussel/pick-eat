b143d2cfa08aeedb6e2073cfaaaa68e99a17c266
Valentin.Roussel <valentin.rssl@gmail.com>
2020-03-24 23:12:23 +0100

    add db conn and store it as data in webserver's app
    this should allows request handlers to access DB

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-use actix_web::{web, App, HttpServer};
 extern crate actix_web;
+extern crate tokio_postgres;
+
+use actix_web::{web, App, HttpServer};
+use tokio_postgres::{NoTls, Error};
+use tokio;
 mod resources;
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
+
+async fn start_web_server(db_conn: tokio_postgres::Client) -> std::io::Result<()> {
+    let mydata = web::Data::new(db_conn);
+    HttpServer::new(move || App::new()
+        .app_data(mydata.clone())
         .service(web::scope("/v1/")
             .configure(resources::recipes::config)
             .configure(resources::ingredients::config)
@@ -13,11 +19,25 @@ async fn main() -> std::io::Result<()> {
 //            .configure(resources::search::config)
         )
     )
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    .bind("127.0.0.1:8080")?.run().await
 }
 
+async fn get_db_conn() -> Result<tokio_postgres::Client, tokio_postgres::Error> {
+    let (client, connection) =
+        tokio_postgres::connect("host=localhost user=valentin dbname=pick_eat", NoTls).await?;
 
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    Ok(client)
+}
 
+#[actix_rt::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
+    let db_conn = get_db_conn().await?;
+    start_web_server(db_conn).await?;
+    Ok(())
+}
