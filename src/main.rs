a11425871ca3626251e7792efbc669e91f35de73
Valentin.Roussel <valentin.rssl@gmail.com>
2020-04-26 01:08:03 +0200

    setup logging with log and simplelog crates

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 extern crate actix_web;
 extern crate tokio_postgres;
+extern crate simplelog;
+extern crate log;
 
 use actix_web::{web, App, HttpServer};
 use tokio_postgres::{NoTls, Error};
+use log::*;
+use simplelog::*;
 use tokio;
+
 mod resources;
 mod handlers;
 
@@ -36,8 +41,37 @@ async fn get_db_conn() -> Result<tokio_postgres::Client, tokio_postgres::Error> 
     Ok(client)
 }
 
+fn setup_logging() {
+    let pickeat_log_config = ConfigBuilder::new()
+        .set_location_level(LevelFilter::Error)
+        .add_filter_allow_str("pick_eat")
+        .build();
+
+    let others_log_config = ConfigBuilder::new()
+        .add_filter_ignore_str("pick_eat")
+        .build();
+
+    let init_log =
+        CombinedLogger::init(
+            vec![
+                TermLogger::new(LevelFilter::Debug, pickeat_log_config.clone(), TerminalMode::Mixed).unwrap(),
+                TermLogger::new(LevelFilter::Warn, others_log_config.clone(), TerminalMode::Mixed).unwrap(),
+            ]
+    );
+    if let Err(_) = init_log {
+        CombinedLogger::init(
+            vec![
+                SimpleLogger::new(LevelFilter::Debug, pickeat_log_config.clone()),
+                SimpleLogger::new(LevelFilter::Warn, others_log_config.clone()),
+            ]
+        ).expect("Could not setup logging");
+    }
+}
+
 #[actix_rt::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    setup_logging();
+    info!("Starting");
     let db_conn = get_db_conn().await?;
     start_web_server(db_conn).await?;
     Ok(())
