10ab59f45ec5163c9d405dd34faa44b2c3dcafd3
Valentin.Roussel <valentin.rssl@gmail.com>
2020-06-14 00:15:02 +0200

    handle PUT requests for recipes
    implementation is ugly though. This needs to be more simple.

--- src/handlers/recipes.rs
+++ src/handlers/recipes.rs
@@ -17,7 +17,7 @@ pub fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(get_all)
        .service(add_one)
        .service(get_one)
-       .service(modify_or_create_one)
+       .service(modify_one)
        .service(delete_one)
     ;
 }
@@ -46,8 +46,7 @@ pub async fn add_one(new_recipe: web::Json<recipe::New>, db_pool: web::Data<Pool
             Ok(rows) => rows[0].get::<&str,i32>("id"),
             Err(ref e) if e.code() == Some(&SqlState::UNIQUE_VIOLATION)
                 //TODO add location with URI or error message
-                => return web::HttpResponse::Conflict()
-                    .finish(),
+                => return web::HttpResponse::Conflict().finish(),
             Err(e) => {
                 error!("{}", e);
                 return web::HttpResponse::InternalServerError().finish();
@@ -236,8 +235,212 @@ pub async fn get_one(id: web::Path<i32>, db_pool: web::Data<Pool>) -> impl Respo
 }
 
 #[put("/recipes/{id}")]
-pub async fn modify_or_create_one(id: web::Path<String>) -> impl Responder {
-    format!("Put recipe {}", id)
+pub async fn modify_one(id: web::Path<i32>, new_recipe: web::Json<recipe::New>, db_pool: web::Data<Pool>) -> impl Responder {
+    let mut db_conn = db_pool.get().await.unwrap();
+    let id = id.into_inner();
+    let transaction = db_conn.transaction().await.expect("Unable to start db transaction");
+    trace!("{:#?}", new_recipe);
+    let recipe_query = "\
+        UPDATE recipes SET \
+            name = $1, \
+            description = $2, \
+            preparation_time_min = $3, \
+            cooking_time_min = $4, \
+            image = $5, \
+            instructions = $6 \
+        WHERE id = $7 \
+        RETURNING id; \
+    ";
+    match transaction.query(recipe_query,
+        &[&new_recipe.name, &new_recipe.desc,
+          &new_recipe.prep_time_min, &new_recipe.cook_time_min,
+          &new_recipe.image, &new_recipe.instructions,
+          &id
+        ]).await {
+            Ok(rows) if rows.len() == 0
+                => return web::HttpResponse::NotFound().finish(),
+            Err(ref e) if e.code() == Some(&SqlState::UNIQUE_VIOLATION)
+                //TODO add location with URI or error message
+                => return web::HttpResponse::Conflict().finish(),
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            Ok(_) => ()
+        };
+
+    //TODO this is ugly and should be more simple
+
+    // Tags
+    if new_recipe.tag_ids.is_empty() {
+        let remove_tags_query = "\
+            DELETE FROM recipes_tags \
+            WHERE recipe_id = $1;
+        ";
+        match transaction.execute(remove_tags_query, &[&id]).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    } else {
+        let query_params = gen_query_params(new_recipe.tag_ids.len(), 2);
+        let insert_tags_query = format!("\
+            INSERT INTO recipes_tags \
+            (tag_id, recipe_id) \
+            VALUES {} \
+            ON CONFLICT DO NOTHING;
+        ", query_params);
+
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = Vec::new();
+        for tag_id in &new_recipe.tag_ids {
+            query_args.extend_from_slice(&[tag_id, &id]);
+        }
+        match transaction.execute(insert_tags_query.as_str(), &query_args).await {
+            Err(ref e) if e.code() == Some(&SqlState::FOREIGN_KEY_VIOLATION)
+                => return web::HttpResponse::UnprocessableEntity().finish(),
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+
+        let query_params = gen_query_params_from(new_recipe.tag_ids.len(), 1, 2);
+        let remove_tags_query = format!("\
+            DELETE FROM recipes_tags \
+            WHERE \
+                recipe_id = $1 \
+                AND tag_id NOT IN ({});
+        ", query_params);
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = vec![&id];
+        for tag_id in &new_recipe.tag_ids {
+            query_args.push(tag_id);
+        }
+        match transaction.execute(remove_tags_query.as_str(), &query_args).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    }
+
+    // Categories
+    if new_recipe.category_ids.is_empty() {
+        let remove_categories_query = "\
+            DELETE FROM recipes_categories \
+            WHERE recipe_id = $1;
+        ";
+        match transaction.execute(remove_categories_query, &[&id]).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    } else {
+        let query_params = gen_query_params(new_recipe.category_ids.len(), 2);
+        let insert_categories_query = format!("\
+            INSERT INTO recipes_categories \
+            (category_id, recipe_id) \
+            VALUES {} \
+            ON CONFLICT DO NOTHING;
+        ", query_params);
+
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = Vec::new();
+        for category_id in &new_recipe.category_ids {
+            query_args.extend_from_slice(&[category_id, &id]);
+        }
+        match transaction.execute(insert_categories_query.as_str(), &query_args).await {
+            Err(ref e) if e.code() == Some(&SqlState::FOREIGN_KEY_VIOLATION)
+                => return web::HttpResponse::UnprocessableEntity().finish(),
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+
+        let query_params = gen_query_params_from(new_recipe.category_ids.len(), 1, 2);
+        let remove_categories_query = format!("\
+            DELETE FROM recipes_categories \
+            WHERE \
+                recipe_id = $1 \
+                AND category_id NOT IN ({});
+        ", query_params);
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = vec![&id];
+        for category_id in &new_recipe.category_ids {
+            query_args.push(category_id);
+        }
+        match transaction.execute(remove_categories_query.as_str(), &query_args).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    }
+
+    // Ingredients
+    if new_recipe.q_ingredient_ids.is_empty() {
+        let remove_ingredients_query = "\
+            DELETE FROM recipes_ingredients \
+            WHERE recipe_id = $1;
+        ";
+        match transaction.execute(remove_ingredients_query, &[&id]).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    } else {
+        let query_params = gen_query_params(new_recipe.q_ingredient_ids.len(), 4);
+        let insert_ingredients_query = format!("\
+            INSERT INTO recipes_ingredients \
+            (recipe_id, ingredient_id, quantity, unit_id) \
+            VALUES {} \
+            ON CONFLICT DO NOTHING;
+        ", query_params);
+
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = Vec::new();
+        for ingr in &new_recipe.q_ingredient_ids {
+            query_args.extend_from_slice(&[&id, &ingr.id, &ingr.quantity, &ingr.unit_id]);
+        }
+        match transaction.execute(insert_ingredients_query.as_str(), &query_args).await {
+            Err(ref e) if e.code() == Some(&SqlState::FOREIGN_KEY_VIOLATION)
+                => return web::HttpResponse::UnprocessableEntity().finish(),
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+
+        let query_params = gen_query_params_from(new_recipe.q_ingredient_ids.len(), 1, 2);
+        let remove_ingredients_query = format!("\
+            DELETE FROM recipes_ingredients \
+            WHERE \
+                recipe_id = $1 \
+                AND ingredient_id NOT IN ({});
+        ", query_params);
+        let mut query_args: Vec<&(dyn ToSql + Sync)> = vec![&id];
+        for ingr in &new_recipe.q_ingredient_ids {
+            query_args.push(&ingr.id);
+        }
+        match transaction.execute(remove_ingredients_query.as_str(), &query_args).await {
+            Err(e) => {
+                error!("{}", e);
+                return web::HttpResponse::InternalServerError().finish();
+            },
+            _ => ()
+        }
+    }
+
+    transaction.commit().await.expect("Error when commiting transaction");
+    web::HttpResponse::Ok().finish()
 }
 
 #[delete("/recipes/{id}")]
