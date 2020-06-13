def9e80b63a5ca878abbf293898d94e6363f9d9e
Valentin.Roussel <valentin.rssl@gmail.com>
2020-06-14 00:12:45 +0200

    add helper function 'gen_query_params_from' for bath insert in DB

--- src/utils.rs
+++ src/utils.rs
@@ -1,8 +1,9 @@
 /// Helper function used for queries with 'batch insert'
-/// gen_query_param(2,3) will return: "($1,$2,$3),($4,$5,$6)"
-pub fn gen_query_params(n_rows: usize, n_params_per_row: usize) -> String {
+/// gen_query_params_from(2,3,1) will return: "($1,$2,$3),($4,$5,$6)"
+/// gen_query_params_from(2,3,2) will return: "($2,$3,$4),($5,$6,$7)"
+pub fn gen_query_params_from(n_rows: usize, n_params_per_row: usize, start_from: usize) -> String {
     let mut rows = Vec::with_capacity(n_rows);
-    let mut n = 1;
+    let mut n = start_from;
     for _ in 0..n_rows {
         let mut row = Vec::with_capacity(n_params_per_row);
         for _ in 0..n_params_per_row {
@@ -14,3 +15,6 @@ pub fn gen_query_params(n_rows: usize, n_params_per_row: usize) -> String {
     rows.join(",")
 }
 
+pub fn gen_query_params(n_rows: usize, n_params_per_row: usize) -> String {
+    gen_query_params_from(n_rows, n_params_per_row, 1)
+}
