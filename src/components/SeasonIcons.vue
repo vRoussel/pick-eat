a2744f7472ef8a59ebe01c5fdb4665d13fedbd42
Valentin Roussel <valentin.rssl@gmail.com>
2021-06-28 20:08:55 +0200

    add season_icons component

---
+++ src/components/SeasonIcons.vue
@@ -0,0 +1,56 @@
+<template>
+    <p >
+        <span class="icon" :class="dynClass(1)">
+          <i class="fas fa-seedling"></i>
+        </span>
+        <span class="icon" :class="dynClass(2)">
+          <i class="fas fa-sun"></i>
+        </span>
+        <span class="icon" :class="dynClass(3)">
+          <i class="fas fa-leaf"></i>
+        </span>
+        <span class="icon" :class="dynClass(4)">
+          <i class="fas fa-snowflake"></i>
+        </span>
+    </p>
+</template>
+
+<script>
+export default {
+    name: 'season-icons',
+    props: {
+        seasons: {
+            type: Array
+        },
+    },
+    methods: {
+        dynClass(id) {
+            return {
+                active : this.has_season(id),
+                inactive : !this.has_season(id),
+                'mx-1' : screen.width <= 760,
+                'mx-3' : screen.width > 760,
+            }
+        },
+        has_season(id) {
+            return this.season_ids.includes(id)
+        },
+    },
+    computed: {
+        season_ids() {
+            return this.seasons.map(r => r.id)
+        },
+    }
+}
+</script>
+
+<style>
+    .inactive {
+        color: lightgrey
+    }
+    
+    .active {
+        color: #BF9C53
+    }
+
+</style>
