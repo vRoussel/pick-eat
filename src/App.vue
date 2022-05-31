d6560d2e70b4093e8bd462193c53255edf4703d2
Valentin Roussel <valentin.rssl@gmail.com>
2022-05-31 07:49:30 +0200

    try to keep component state when switching
    Only works for new-recipe at the moment

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,15 @@
     </div>
   </div>
   </nav>
-  <router-view />
+  <router-view v-slot="{ Component, route }">
+    <keep-alive>
+          <component
+            :is="Component"
+            :key="route.meta.usePathKey ? route.path : undefined"
+          />
+    </keep-alive>
+  </router-view>
+
 </template>
 
 <script>
