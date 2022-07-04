8ad99a546f7552ceb2b26391b3daa7706f306f09
Valentin Roussel <valentin.rssl@gmail.com>
2022-07-04 08:52:00 +0200

    pickeat is now built with docker for deployment

--- backend/build_for_deploy.sh
+++ backend/build_for_deploy.sh
@@ -1,1 +1,9 @@
-cargo install --target x86_64-unknown-linux-musl --root ../ansible/to_deploy/backend/ --path .
+export DOCKER_BUILDKIT=1
+script_dir="$(dirname "$(realpath "$0")")"
+out_dir="../ansible/to_deploy/backend"
+
+echo "Building for arm64"
+(cd "$script_dir"; docker build --platform linux/arm64 -o ${out_dir}/aarch64/ .)
+
+echo "Building for amd64"
+(cd "$script_dir"; docker build --platform linux/amd64 -o ${out_dir}/x86_64/ .)
