7ba11ae9c44ef3d907e221735c9258c43296a6b4
Valentin Roussel <valentin.rssl@gmail.com>
2023-04-14 09:14:44 +0200

    add possibility to show info toast msg in app

--- frontend/src/store/notif.js
+++ frontend/src/store/notif.js
@@ -4,12 +4,17 @@ export const useNotifStore = defineStore('notif', {
     state: () => {
         return {
             error_msgs: new Array(),
+            info_msgs: new Array()
         }
     },
     actions: {
         show_error(msg) {
             this.error_msgs.push(msg)
             setTimeout(() => this.error_msgs.shift(), 5000)
+        },
+        show_info(msg) {
+            this.info_msgs.push(msg)
+            setTimeout(() => this.info_msgs.shift(), 5000)
         }
     }
 })
