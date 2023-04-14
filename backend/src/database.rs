1e875e55a55a39984995d1026598897773f8f9a4
Valentin Roussel <valentin.rssl@gmail.com>
2023-04-14 08:21:48 +0200

    fix PG uri when using unix domain socket

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -3,12 +3,12 @@ use sqlx::postgres::{PgPool, PgPoolOptions};
 
 pub async fn get_pool(db_conf: &DBConf) -> Result<PgPool, Box<dyn std::error::Error>> {
     let conn_str = format!(
-        "postgres://{}:{}@{}:{}/{}",
-        db_conf.user,
-        db_conf.password.clone().unwrap_or_default(),
+        "postgres:///{}?host={}&port={}&user={}&password={}",
+        db_conf.dbname,
         db_conf.host,
         db_conf.port.unwrap_or(5432).to_string(),
-        db_conf.dbname
+        db_conf.user,
+        db_conf.password.clone().unwrap_or_default(),
     );
 
     let pool = PgPoolOptions::new()
