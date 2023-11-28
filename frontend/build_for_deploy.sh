7846a975faca74d1cff420fcd0c76a80031ffbd0
Valentin Roussel <valentin.rssl@gmail.com>
2023-11-28 21:32:15 +0100

    enable static brotli and gzip in nginx conf

--- frontend/build_for_deploy.sh
+++ frontend/build_for_deploy.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 npm run build -- --outDir ../ansible/to_deploy/frontend --emptyOutDir
+find ../ansible/to_deploy/frontend/ \( -name '*.js' -o -name '*.css' -o -name '*.html' \) -size +1400c -exec sh -c 'brotli --keep --best "$1"; gzip --keep --best "$1"' shell {} \;
