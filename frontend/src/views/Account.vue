23b83db82c71afdbec61e49ab7317af7d6afe482
Valentin Roussel <valentin.rssl@gmail.com>
2023-12-09 09:16:17 +0100

    convert account.vue to composition API

--- frontend/src/views/Account.vue
+++ frontend/src/views/Account.vue
@@ -1,32 +1,27 @@
+<script setup>
+import AccountUpdateForm from '@/components/AccountUpdateForm.vue'
+import AccountView from '@/components/AccountView.vue'
+import { useRouter, useRoute } from 'vue-router'
+
+const router = useRouter()
+const route = useRoute()
+
+defineProps({
+    edit: Boolean
+})
+
+function editAccount() {
+    router.push({ query: { ...route.query, edit: null } })
+}
+
+function afterEdit() {
+    router.push({ query: { ...route.query, edit: undefined } })
+}
+</script>
+
 <template>
     <div class="container my-8">
         <account-view v-if="!edit" @edit="editAccount" />
         <account-update-form v-else @done="afterEdit" />
     </div>
 </template>
-
-<script>
-import AccountUpdateForm from '@/components/AccountUpdateForm.vue'
-import AccountView from '@/components/AccountView.vue'
-
-export default {
-    name: 'Account',
-    components: {
-        AccountUpdateForm,
-        AccountView,
-    },
-    props: {
-        edit: {
-            type: Boolean,
-        },
-    },
-    methods: {
-        editAccount() {
-            this.$router.push({ query: { ...this.$route.query, edit: null } })
-        },
-        afterEdit() {
-            this.$router.push({ query: { ...this.$route.query, edit: undefined } })
-        },
-    },
-}
-</script>
