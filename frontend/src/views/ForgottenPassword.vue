9bcec5dc0002419ac3d95f263b3373a81d3e86b8
Valentin Roussel <valentin.rssl@gmail.com>
2023-12-09 12:35:19 +0100

    convert forgottenpassword.vue to composition api

--- frontend/src/views/ForgottenPassword.vue
+++ frontend/src/views/ForgottenPassword.vue
@@ -1,9 +1,79 @@
+<script setup>
+import { onMounted, nextTick, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { object, string } from 'yup'
+
+import { useAuthStore } from '@/store/auth.js'
+import { useNotifStore } from '@/store/notif.js'
+
+import { handle_form_api_errors, handle_form_local_errors } from '@/utils/utils.js'
+
+
+const notifStore = useNotifStore()
+const authStore = useAuthStore()
+const router = useRouter()
+
+const fields = ref({
+    email: null
+})
+const errors = ref({
+    email: null
+})
+const validator = object().shape({
+    email: string()
+        .required("L'adresse mail est obligatoire")
+        .email("L'adresse mail est invalide")
+})
+
+const email_input_el = ref(null)
+onMounted(async () => {
+    await nextTick()
+    email_input_el.value.focus()
+})
+
+
+async function send_password_reset_token_request() {
+    validator
+        .validate(fields.value, { abortEarly: false })
+        .then(() => {
+            errors.value = {}
+            authStore
+                .ask_password_reset_token(fields.value.email)
+                .then(() => {
+                    notifStore.show_info(
+                        "Si l'adresse mail correspond bien a votre compte, un email vous a été envoyé",
+                    )
+                    fields.value.email = null
+                    router.push('/login')
+                })
+                .catch((err) => {
+                    handle_form_api_errors(err.response, errors.value, notifStore)
+                })
+        })
+        .catch((err) => {
+            handle_form_local_errors(err.inner, errors.value, notifStore)
+        })
+}
+function validate(field) {
+    validator
+        .validateAt(field, fields.value)
+        .then(() => {
+            errors.value[field] = null
+        })
+        .catch((err) => {
+            setTimeout(() => (errors.value[field] = err.message), 200)
+        })
+}
+function resend_validation_email() {
+    this.authStore.ask_account_validation_token(fields.value.email)
+    this.notifStore.show_info('Nouvel email de confirmation envoyé')
+}
+</script>
+
 <template>
     <div class="my-8">
-        <form
-            class="mx-auto space-y-4 p-8 border-primary border-[1px] rounded-xl max-w-md"
-            @submit.prevent="send_password_reset_token_request"
-        >
+        <form class="mx-auto space-y-4 p-8 border-primary border-[1px] rounded-xl max-w-md"
+            @submit.prevent="send_password_reset_token_request">
             <h1 class="text-xl font-bold text-center">Mot de passe oublié</h1>
             <p class="text-center">
                 Entrez votre email pour recevoir un lien qui vous permettra de choisir un nouveau
@@ -13,13 +83,8 @@
                 <label class="label">
                     <span class="label-text">Adresse mail</span>
                 </label>
-                <input
-                    ref="email"
-                    v-model="email"
-                    type="text"
-                    class="input input-bordered w-full"
-                    :class="errors.email && '!input-error'"
-                />
+                <input ref="email_input_el" v-model="fields.email" type="text" class="input input-bordered w-full"
+                    :class="errors.email && '!input-error'" />
                 <label v-if="errors.email" class="label">
                     <span class="label-text-alt text-error">{{ errors.email }}</span>
                 </label>
@@ -33,71 +98,3 @@
         </form>
     </div>
 </template>
-
-<script>
-import { mapStores } from 'pinia'
-import { useAuthStore } from '@/store/auth.js'
-import { useNotifStore } from '@/store/notif.js'
-import { handle_form_api_errors, handle_form_local_errors } from '@/utils/utils.js'
-import { object, string } from 'yup'
-
-const validator = object().shape({
-    email: string().required("L'adresse mail est obligatoire").email("L'adresse mail est invalide"),
-})
-
-export default {
-    name: 'ForgottenPassword',
-    data: function () {
-        return {
-            email: null,
-            errors: {
-                email: null,
-            },
-        }
-    },
-    computed: {
-        ...mapStores(useAuthStore, useNotifStore),
-    },
-    mounted() {
-        this.$nextTick(() => this.$refs.email.focus())
-    },
-    methods: {
-        async send_password_reset_token_request() {
-            validator
-                .validate(this, { abortEarly: false })
-                .then(() => {
-                    this.errors = {}
-                    this.authStore
-                        .ask_password_reset_token(this.email)
-                        .then(() => {
-                            this.notifStore.show_info(
-                                "Si l'adresse mail correspond bien a votre compte, un email vous a été envoyé",
-                            )
-                            this.email = null
-                            this.$router.push('/login')
-                        })
-                        .catch((err) => {
-                            handle_form_api_errors(err.response, this.errors, this.notifStore)
-                        })
-                })
-                .catch((err) => {
-                    handle_form_local_errors(err.inner, this.errors, this.notifStore)
-                })
-        },
-        validate(field) {
-            validator
-                .validateAt(field, this)
-                .then(() => {
-                    this.errors[field] = null
-                })
-                .catch((err) => {
-                    setTimeout(() => (this.errors[field] = err.message), 200)
-                })
-        },
-        resend_validation_email() {
-            this.authStore.ask_account_validation_token(this.email)
-            this.notifStore.show_info('Nouvel email de confirmation envoyé')
-        },
-    },
-}
-</script>
