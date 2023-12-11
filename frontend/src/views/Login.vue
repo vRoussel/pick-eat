6a9a963f0805cfa9ddbf2a75db91c72f4ba101df
Valentin Roussel <valentin.rssl@gmail.com>
2023-12-11 08:56:31 +0100

    convert login.vue to composition api

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,78 @@
+<script setup>
+import { onMounted, nextTick, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { object, string } from 'yup'
+
+import { useAuthStore } from '@/store/auth.js'
+import { useNotifStore } from '@/store/notif.js'
+
+import { handle_form_api_errors, handle_form_local_errors } from '@/utils/utils.js'
+
+const notifStore = useNotifStore()
+const authStore = useAuthStore()
+const router = useRouter()
+
+const fields = ref({
+    email: null,
+    password: null,
+})
+const errors = ref({
+    email: null,
+    password: null,
+})
+const validator = object().shape({
+    email: string()
+        .required("L'adresse mail est obligatoire")
+        .email("L'adresse mail est invalide"),
+    password: string()
+        .required('Le mot de passe est obligatoire'),
+})
+
+const email_input_el = ref(null)
+onMounted(async () => {
+    await nextTick()
+    email_input_el.value.focus()
+})
+
+const show_resend_validation_email = ref(false)
+async function login() {
+    validator
+        .validate(fields.value, { abortEarly: false })
+        .then(() => {
+            errors.value = {}
+            authStore
+                .login(fields.value.email, fields.value.password)
+                .then(() => {
+                    router.push(authStore.return_url || '/account')
+                    authStore.return_url = null
+                    fields.value.email = null
+                    fields.value.password = null
+                })
+                .catch((err) => {
+                    handle_form_api_errors(err.response, errors.value, notifStore)
+                    if (
+                        err.response.data.errors.find(
+                            (x) => x.code == 'account_pending_validation',
+                        )
+                    )
+                        show_resend_validation_email = true
+                })
+        })
+        .catch((err) => {
+            handle_form_local_errors(err.inner, errors.value, notifStore)
+        })
+}
+
+function resend_validation_email() {
+    show_resend_validation_email = false
+    authStore.ask_account_validation_token(fields.value.email)
+    notifStore.show_info('Nouvel email de confirmation envoyé')
+}
+</script>
+
 <template>
     <div class="my-8">
-        <form
-            class="mx-auto space-y-4 p-8 border-primary border-[1px] rounded-xl max-w-md"
-            @submit.prevent="login"
-        >
+        <form class="mx-auto space-y-4 p-8 border-primary border-[1px] rounded-xl max-w-md" @submit.prevent="login">
             <h1 class="text-xl font-bold text-center">Connexion</h1>
             <p class="text-center">
                 Vous n'avez pas encore de compte ?
@@ -11,21 +80,14 @@
             </p>
             <p v-if="show_resend_validation_email" class="text-center">
                 Vous n'avez pas reçu l'email de validation ?
-                <span class="link link-primary no-underline" @click="resend_validation_email"
-                    >Renvoyer</span
-                >
+                <span class="link link-primary no-underline" @click="resend_validation_email">Renvoyer</span>
             </p>
             <div class="form-control">
                 <label class="label">
                     <span class="label-text">Adresse mail</span>
                 </label>
-                <input
-                    ref="email"
-                    v-model="email"
-                    type="text"
-                    class="input input-bordered w-full"
-                    :class="errors.email && '!input-error'"
-                />
+                <input ref="email_input_el" v-model="fields.email" type="text" class="input input-bordered w-full"
+                    :class="errors.email && '!input-error'" />
                 <label v-if="errors.email" class="label">
                     <span class="label-text-alt text-error">{{ errors.email }}</span>
                 </label>
@@ -33,19 +95,11 @@
             <div class="form-control">
                 <label class="label">
                     <span class="label-text">Mot de passe</span>
-                    <router-link
-                        tabindex="-1"
-                        class="label-text-alt text-sm text-primary cursor-pointer"
-                        to="/forgotten_password"
-                        >Mot de passe oublié</router-link
-                    >
+                    <router-link tabindex="-1" class="label-text-alt text-sm text-primary cursor-pointer"
+                        to="/forgotten_password">Mot de passe oublié</router-link>
                 </label>
-                <input
-                    v-model="password"
-                    type="password"
-                    class="input input-bordered w-full"
-                    :class="errors.password && '!input-error'"
-                />
+                <input v-model="fields.password" type="password" class="input input-bordered w-full"
+                    :class="errors.password && '!input-error'" />
                 <label v-if="errors.password" class="label">
                     <span class="label-text-alt text-error">{{ errors.password }}</span>
                 </label>
@@ -57,80 +111,3 @@
     </div>
 </template>
 
-<script>
-import { mapStores } from 'pinia'
-import { useAuthStore } from '@/store/auth.js'
-import { useNotifStore } from '@/store/notif.js'
-import { handle_form_api_errors, handle_form_local_errors } from '@/utils/utils.js'
-import { object, string } from 'yup'
-
-const validator = object().shape({
-    email: string().required("L'adresse mail est obligatoire").email("L'adresse mail est invalide"),
-    password: string().required('Le mot de passe est obligatoire'),
-})
-
-export default {
-    name: 'Login',
-    data: function () {
-        return {
-            email: null,
-            password: null,
-            show_resend_validation_email: false,
-            errors: {
-                email: null,
-                password: null,
-            },
-        }
-    },
-    computed: {
-        ...mapStores(useAuthStore, useNotifStore),
-    },
-    mounted() {
-        this.$nextTick(() => this.$refs.email.focus())
-    },
-    methods: {
-        async login() {
-            validator
-                .validate(this, { abortEarly: false })
-                .then(() => {
-                    this.errors = {}
-                    this.authStore
-                        .login(this.email, this.password)
-                        .then(() => {
-                            this.$router.push(this.authStore.return_url || '/account')
-                            this.authStore.return_url = null
-                            this.email = null
-                            this.password = null
-                        })
-                        .catch((err) => {
-                            handle_form_api_errors(err.response, this.errors, this.notifStore)
-                            if (
-                                err.response.data.errors.find(
-                                    (x) => x.code == 'account_pending_validation',
-                                )
-                            )
-                                this.show_resend_validation_email = true
-                        })
-                })
-                .catch((err) => {
-                    handle_form_local_errors(err.inner, this.errors, this.notifStore)
-                })
-        },
-        validate(field) {
-            validator
-                .validateAt(field, this)
-                .then(() => {
-                    this.errors[field] = null
-                })
-                .catch((err) => {
-                    setTimeout(() => (this.errors[field] = err.message), 200)
-                })
-        },
-        resend_validation_email() {
-            this.show_resend_validation_email = false
-            this.authStore.ask_account_validation_token(this.email)
-            this.notifStore.show_info('Nouvel email de confirmation envoyé')
-        },
-    },
-}
-</script>
