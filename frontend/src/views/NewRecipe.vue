de93fcfe136e8e5d5039888b89ff4b8253ed0fc9
Valentin Roussel <valentin.rssl@gmail.com>
2023-12-12 08:36:22 +0100

    convert newrecipe.vue to composition api

--- frontend/src/views/NewRecipe.vue
+++ frontend/src/views/NewRecipe.vue
@@ -1,30 +1,25 @@
+<script setup>
+import { onActivated } from 'vue'
+import { useRouter } from 'vue-router'
+
+import RecipeForm from '@/components/RecipeForm.vue'
+import { useFoodStore } from '@/store/food.js'
+
+const foodStore = useFoodStore()
+const router = useRouter()
+
+function viewRecipe(recipe) {
+    let id = recipe.id
+    router.push({ name: 'recipe', params: { id } })
+}
+onActivated(() => {
+    foodStore.fetchAll()
+})
+</script>
+
 <template>
     <div>
-        <recipe-form @done="afterInsert" />
+        <recipe-form @done="viewRecipe" />
     </div>
 </template>
 
-<script>
-import RecipeForm from '@/components/RecipeForm.vue'
-import { mapStores } from 'pinia'
-import { useFoodStore } from '@/store/food.js'
-
-export default {
-    name: 'NewRecipe',
-    components: {
-        RecipeForm,
-    },
-    methods: {
-        afterInsert(recipe) {
-            let id = recipe.id
-            this.$router.push({ name: 'recipe', params: { id } })
-        },
-    },
-    computed: {
-        ...mapStores(useFoodStore),
-    },
-    activated() {
-        this.foodStore.fetchAll()
-    },
-}
-</script>
