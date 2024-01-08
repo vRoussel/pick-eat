d655b66d51693761f8721ac8a35fc6a761af6a13
Valentin Roussel <valentin.rssl@gmail.com>
2024-01-08 08:33:02 +0100

    convert recipefilters.vue to composition api

--- frontend/src/components/RecipeFilters.vue
+++ frontend/src/components/RecipeFilters.vue
@@ -1,3 +1,149 @@
+<script setup>
+import { inject, ref, computed, onMounted } from 'vue'
+
+import Multiselect from '@vueform/multiselect'
+
+import { useFoodStore } from '@/store/food.js'
+
+const foodStore = useFoodStore()
+
+const icons = inject('icons')
+
+const model = defineModel('filters', {
+    required: true,
+    type: Object
+})
+
+const timer = ref(null)
+const expanded = ref(false)
+const innerWidth_cached = ref(0)
+
+const search_query = computed({
+    get: function () {
+        return model.value.search_query
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, search_query: val }
+        if (val == '' || val == null) updateFilters(new_filters, 0)
+        else updateFilters(new_filters, 400)
+    }
+})
+
+const categories = computed({
+    get: function () {
+        return model.value.categories
+    },
+    set: function (val) {
+        let new_filters = { ...model.value.filters, categories: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+const tags = computed({
+    get: function () {
+        return model.value.tags
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, tags: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+const ingredients = computed({
+    get: function () {
+        return model.value.ingredients
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, ingredients: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+const seasons = computed({
+    get: function () {
+        return model.value.seasons
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, seasons: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+const account = computed({
+    get: function () {
+        return model.value.account
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, account: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+const diets = computed({
+    get: function () {
+        return model.value.diets
+    },
+    set: function (val) {
+        let new_filters = { ...model.value, diets: val }
+        updateFilters(new_filters, 0)
+    },
+})
+
+onMounted(() => {
+    expanded.value = !on_mobile();
+    innerWidth_cached.value = window.innerWidth
+    window.addEventListener('resize', () => {
+        let old_val = innerWidth_cached.value
+        let new_val = window.innerWidth
+        if (old_val <= 768 && new_val > 768)
+            expanded.value = true
+        else if (old_val > 768 && new_val <= 768)
+            expanded.value = false
+        innerWidth_cached.value = new_val
+    })
+})
+
+function updateFilters(filters, delay) {
+    if (timer.value) {
+        clearTimeout(timer.value)
+        timer.value = null
+    }
+    timer.value = setTimeout(() => {
+        model.value = filters
+    }, delay)
+}
+
+function toggle() {
+    expanded.value = !expanded.value
+}
+
+function clearSearch() {
+    search_query.value = null
+}
+
+function clearFilters() {
+    updateFilters(Filters(), 0)
+}
+function on_mobile() {
+    return window.innerWidth < 768
+}
+</script>
+
+//Maybe this should be in a dedicated .js file, I'm not sure
+<script>
+export function Filters(q = null, i = [], t = [], c = [], s = [], a = null, d = []) {
+    return {
+        search_query: q,
+        ingredients: i,
+        tags: t,
+        categories: c,
+        seasons: s,
+        account: a,
+        diets: d,
+    }
+}
+</script>
+
 <template>
     <div>
         <div class="flex gap-x-2">
@@ -13,20 +159,11 @@
                     <span class="icon text-xl bg-accent text-accent-content join px-3">
                         <Icon :icon="icons.search" :inline="true" />
                     </span>
-                    <input
-                        class="input w-full join-item"
-                        type="search"
-                        placeholder="Trouver une recette"
-                        :value="search_query"
-                        @input="(e) => (search_query = e.target.value)"
-                    />
+                    <input class="input w-full join-item" type="search" placeholder="Trouver une recette"
+                        :value="search_query" @input="(e) => (search_query = e.target.value)" />
                 </div>
-                <span v-if="filters.search_query" class="icon cursor-pointer" @click="clearSearch">
-                    <Icon
-                        :icon="icons.close"
-                        class="absolute right-3 top-0 bottom-0 h-full"
-                        @click="clearSearch"
-                    />
+                <span v-if="model.search_query" class="icon cursor-pointer" @click="clearSearch">
+                    <Icon :icon="icons.close" class="absolute right-3 top-0 bottom-0 h-full" @click="clearSearch" />
                 </span>
             </div>
         </div>
@@ -35,33 +172,15 @@
                 <label class="label">
                     <span class="label-text">Ingrédients</span>
                 </label>
-                <Multiselect
-                    v-model="ingredients"
-                    mode="tags"
-                    :options="foodStore.ingredients"
-                    label="name"
-                    searchable
-                    :strict="false"
-                    track-by="name"
-                    value-prop="id"
-                    :close-on-select="false"
-                />
+                <Multiselect v-model="ingredients" mode="tags" :options="foodStore.ingredients" label="name" searchable
+                    :strict="false" track-by="name" value-prop="id" :close-on-select="false" />
             </div>
             <div class="form-control">
                 <label class="label">
                     <span class="label-text">Tags</span>
                 </label>
-                <Multiselect
-                    v-model="tags"
-                    mode="tags"
-                    :options="foodStore.tags"
-                    label="name"
-                    searchable
-                    :strict="false"
-                    track-by="name"
-                    value-prop="id"
-                    :close-on-select="false"
-                />
+                <Multiselect v-model="tags" mode="tags" :options="foodStore.tags" label="name" searchable :strict="false"
+                    track-by="name" value-prop="id" :close-on-select="false" />
             </div>
             <!--
         <fieldset>
@@ -82,12 +201,7 @@
                 </legend>
                 <div v-for="d in foodStore.diets" :key="d.id" class="form-control">
                     <label class="label cursor-pointer justify-start gap-x-4 py-1">
-                        <input
-                            v-model="diets"
-                            type="checkbox"
-                            class="checkbox checkbox-sm checkbox-accent"
-                            :value="d.id"
-                        />
+                        <input v-model="diets" type="checkbox" class="checkbox checkbox-sm checkbox-accent" :value="d.id" />
                         <span class="label-text">{{ d.name }}</span>
                     </label>
                 </div>
@@ -98,12 +212,8 @@
                 </legend>
                 <div v-for="s in foodStore.seasons" :key="s.id" class="form-control">
                     <label class="label cursor-pointer justify-start gap-x-4 py-1">
-                        <input
-                            v-model="seasons"
-                            type="checkbox"
-                            class="checkbox checkbox-sm checkbox-accent"
-                            :value="s.id"
-                        />
+                        <input v-model="seasons" type="checkbox" class="checkbox checkbox-sm checkbox-accent"
+                            :value="s.id" />
                         <span class="label-text">{{ s.name }}</span>
                     </label>
                 </div>
@@ -114,12 +224,8 @@
                 </legend>
                 <div v-for="c in foodStore.categories" :key="c.id" class="form-control">
                     <label class="label cursor-pointer justify-start gap-x-4 py-1">
-                        <input
-                            v-model="categories"
-                            type="checkbox"
-                            class="checkbox checkbox-sm checkbox-accent"
-                            :value="c.id"
-                        />
+                        <input v-model="categories" type="checkbox" class="checkbox checkbox-sm checkbox-accent"
+                            :value="c.id" />
                         <span class="label-text">{{ c.name }}</span>
                     </label>
                 </div>
@@ -128,17 +234,8 @@
                 <label class="label">
                     <span class="label-text">Auteur</span>
                 </label>
-                <Multiselect
-                    v-model="account"
-                    mode="single"
-                    :options="foodStore.accounts_with_recipes"
-                    label="display_name"
-                    searchable
-                    :strict="false"
-                    track-by="display_name"
-                    value-prop="id"
-                    :close-on-select="true"
-                />
+                <Multiselect v-model="account" mode="single" :options="foodStore.accounts_with_recipes" label="display_name"
+                    searchable :strict="false" track-by="display_name" value-prop="id" :close-on-select="true" />
             </div>
             <div class="form-control">
                 <button class="btn btn-accent" @click="clearFilters">
@@ -151,142 +248,3 @@
     </div>
 </template>
 
-<script>
-import Multiselect from '@vueform/multiselect'
-
-import { mapStores } from 'pinia'
-import { useFoodStore } from '@/store/food.js'
-
-export function Filters(q = null, i = [], t = [], c = [], s = [], a = null, d = []) {
-    return {
-        search_query: q,
-        ingredients: i,
-        tags: t,
-        categories: c,
-        seasons: s,
-        account: a,
-        diets: d,
-    }
-}
-
-export default {
-    name: 'RecipeFilters',
-    components: {
-        Multiselect,
-    },
-    inject: ['icons'],
-    props: {
-        filters: {
-            type: Object,
-            required: true,
-        },
-    },
-    emits: ['update:filters'],
-    data: function () {
-        return {
-            timer: null,
-            expanded: !this.on_mobile(),
-            innerWidth_cached: window.innerWidth,
-        }
-    },
-    computed: {
-        ...mapStores(useFoodStore),
-        search_query: {
-            get: function () {
-                return this.filters.search_query
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, search_query: val }
-                if (val == '' || val == null) this.updateFilters(new_filters, 0)
-                else this.updateFilters(new_filters, 400)
-            },
-        },
-        tags: {
-            get: function () {
-                return this.filters.tags
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, tags: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-        categories: {
-            get: function () {
-                return this.filters.categories
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, categories: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-        seasons: {
-            get: function () {
-                return this.filters.seasons
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, seasons: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-        ingredients: {
-            get: function () {
-                return this.filters.ingredients
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, ingredients: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-        account: {
-            get: function () {
-                return this.filters.account
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, account: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-        diets: {
-            get: function () {
-                return this.filters.diets
-            },
-            set: function (val) {
-                let new_filters = { ...this.filters, diets: val }
-                this.updateFilters(new_filters, 0)
-            },
-        },
-    },
-    mounted() {
-        window.addEventListener('resize', () => {
-            let old_val = this.innerWidth_cached
-            let new_val = window.innerWidth
-            if (old_val <= 768 && new_val > 768) this.expanded = true
-            else if (old_val > 768 && new_val <= 768) this.expanded = false
-            this.innerWidth_cached = new_val
-        })
-    },
-    methods: {
-        updateFilters(filters, delay) {
-            if (this.timer) {
-                clearTimeout(this.timer)
-                this.timer = null
-            }
-            this.timer = setTimeout(() => {
-                this.$emit('update:filters', filters)
-            }, delay)
-        },
-        toggle() {
-            this.expanded = !this.expanded
-        },
-        clearSearch() {
-            this.search_query = null
-        },
-        clearFilters() {
-            this.updateFilters(Filters(), 0)
-        },
-        on_mobile() {
-            return window.innerWidth < 768
-        },
-    },
-}
-</script>
