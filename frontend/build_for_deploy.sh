b4e9e6ea1a992add736e380ab00f175034df2558
Valentin Roussel <valentin.rssl@gmail.com>
2024-01-25 20:16:46 +0100

    fix build_for_deploy.sh

--- frontend/build_for_deploy.sh
+++ frontend/build_for_deploy.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-npm run build -- --outDir ../ansible/to_deploy/frontend --emptyOutDir
+npm run build
+mkdir -p ../ansible/to_deploy/frontend
+rm -rf ../ansible/to_deploy/frontend/*
+mv dist/* ../ansible/to_deploy/frontend
 find ../ansible/to_deploy/frontend/ \( -name '*.js' -o -name '*.css' -o -name '*.html' \) -size +1400c -exec sh -c 'brotli --keep --best "$1"; gzip --keep --best "$1"' shell {} \;
