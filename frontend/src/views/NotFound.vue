b9fa0efc65e652ffad5ccfa665795407a74792bd
Valentin Roussel <valentin.rssl@gmail.com>
2024-01-31 08:13:37 +0100

    add soft 404 page

---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useHead } from '@unhead/vue'
+
+import no_recipe_video from '@/assets/homer_not_found.mp4'
+
+useHead({
+    title: "404 not found",
+    meta: {
+        name: 'robots',
+        content: 'noindex'
+    }
+})
+</script>
+
+<template>
+    <div>
+        <p class="text-xl mb-2">Oups, cette page n'existe pas</p>
+        <video :src="no_recipe_video" autoplay loop muted playsinline />
+    </div>
+</template>
