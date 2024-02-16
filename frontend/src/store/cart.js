9e3584c27c6b0748810c287e27dae01153b976cb
Valentin Roussel <valentin.rssl@gmail.com>
2024-02-16 07:31:00 +0100

    convert cart store to "composition api" style

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,46 +1,43 @@
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCartStore = defineStore('cart', {
-    state: () => {
-        return {
-            content: new Map(),
+export const useCartStore = defineStore('cart', () => {
+    const content = ref(new Map())
+
+    let recipeCount = computed(() => {
+        return content.value.size
+    })
+
+    function addRecipe(recipe, shares) {
+        content.value.set(recipe.id, { recipe: recipe, shares: shares })
+        save_cart(content.value)
+    }
+
+    function removeRecipe(r_id) {
+        content.value.delete(r_id)
+        save_cart(content.value)
+    }
+
+    function hasRecipe(r_id) {
+        return content.value.has(r_id)
+    }
+
+    function updateRecipeShares(r_id, shares) {
+        content.value.get(r_id).shares = shares
+        save_cart(content.value)
+    }
+
+    function restore() {
+        try {
+            let saved = new Map(JSON.parse(localStorage.getItem("cart")))
+            if (saved)
+                content.value = saved
+        } catch {
+            console.error("Unable to restore cart from local storage")
         }
-    },
-    getters: {
-        recipeCount(state) {
-            return state.content.size
-        },
-    },
-    actions: {
-        addRecipe(recipe, shares) {
-            this.content.set(recipe.id, { recipe: recipe, shares: shares })
-            save_cart(this.content)
-        },
+    }
 
-        removeRecipe(r_id) {
-            this.content.delete(r_id)
-            save_cart(this.content)
-        },
-
-        hasRecipe(r_id) {
-            return this.content.has(r_id)
-        },
-
-        updateRecipeShares(r_id, shares) {
-            this.content.get(r_id).shares = shares
-            save_cart(this.content)
-        },
-
-        restore() {
-            try {
-                let saved = new Map(JSON.parse(localStorage.getItem("cart")))
-                if (saved)
-                    this.content = saved
-            } catch {
-                console.error("Unable to restore cart from local storage")
-            }
-        }
-    },
+    return { content, recipeCount, addRecipe, removeRecipe, hasRecipe, updateRecipeShares, restore }
 })
 
 function save_cart(content) {
