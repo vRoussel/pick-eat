0d7d3e3c2dae7f35a88bbebeb160655c5901e691
Valentin Roussel <valentin.rssl@gmail.com>
2024-02-17 18:59:28 +0100

    restore cart from api when logged in
    if there was also a cart in localstore, merge them

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,14 +1,16 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import debounce from 'lodash.debounce'
 
 import { useNotifStore } from '@/store/notif.js'
-import { useFoodStore } from '@/store/food.js'
+import { useAuthStore } from '@/store/auth.js'
 
 export const useCartStore = defineStore('cart', () => {
     const notifStore = useNotifStore()
-    const foodStore = useFoodStore()
+    const authStore = useAuthStore()
 
     const content = ref(new Map())
+    const last_update = ref(null)
 
     let recipeCount = computed(() => {
         return content.value.size
@@ -20,11 +22,13 @@ export const useCartStore = defineStore('cart', () => {
             return
         }
         content.value.set(recipe.id, { recipe: recipe, shares: shares })
+        last_update.value = Date.now()
         backup()
     }
 
     function removeRecipe(r_id) {
         content.value.delete(r_id)
+        last_update.value = Date.now()
         backup()
     }
 
@@ -34,6 +38,7 @@ export const useCartStore = defineStore('cart', () => {
 
     function updateRecipeShares(r_id, shares) {
         content.value.get(r_id).shares = shares
+        last_update.value = Date.now()
         backup()
     }
 
@@ -43,29 +48,73 @@ export const useCartStore = defineStore('cart', () => {
             backup()
     }
 
-    function backup() {
-        localStorage.setItem("cart", JSON.stringify([...content.value]))
+    var save_account_data_debounced = debounce(async (data) => {
+        await authStore.save_account_data('grocery_list', data)
+    }, 5000)
+
+    async function backup() {
+        let data = JSON.stringify({ content: [...content.value], last_update: last_update.value })
+        backup_local(data)
+        if (authStore.is_logged_in) {
+            save_account_data_debounced(data)
+        }
+    }
+
+    function backup_local(data) {
+        localStorage.setItem("cart", data)
     }
 
     async function restore() {
+        let backup, local_backup, api_backup
         try {
-            let saved = new Map(JSON.parse(localStorage.getItem("cart")))
-            if (saved) {
-                content.value = saved
-            }
-        } catch {
-            console.error("Unable to restore cart from local storage")
+            local_backup = get_local_backup();
+        } catch (e) {
+            local_backup = null
+            console.error("Unable to restore cart from local storage and/or api")
         }
 
-        try {
-            let ids = Array.from(content.value.keys())
-            let up_to_date_recipes = await foodStore.getRecipesFromIds(ids)
-            for (const r of up_to_date_recipes) {
-                updateRecipe(r.id, r, should_backup = false)
+        if (authStore.is_logged_in) {
+            try {
+                api_backup = await authStore.get_account_data('grocery_list')
+                api_backup.content = new Map(api_backup.content)
+            } catch (e) {
+                api_backup = null
+                console.error("Unable to restore cart from local storage and/or api")
             }
-            backup()
-        } catch (e) {
-            console.error("Unable to update cart recipes from API")
+        }
+
+        backup = merge_backups(local_backup, api_backup)
+        if (backup) {
+            content.value = backup.content
+            last_update.value = backup.last_update
+        }
+    }
+
+    function get_local_backup() {
+        let tmp = JSON.parse(localStorage.getItem("cart"))
+        tmp.content = new Map(tmp.content)
+    }
+
+    function merge_backups(cart1, cart2) {
+        let merged_last_update
+        let merged_content
+        if (!cart1 && !cart2)
+            return null
+        else if (!cart1)
+            return cart2
+        else if (!cart2)
+            return cart1
+
+        merged_last_update = Math.max(cart1.last_update, cart2.last_update)
+        if (cart1.last_update >= cart2.last_update) {
+            merged_content = new Map([...cart2.content, ...cart1.content])
+        } else {
+            merged_content = new Map([...cart1.content, ...cart2.content])
+        }
+
+        return {
+            last_update: merged_last_update,
+            content: merged_content
         }
     }
 
