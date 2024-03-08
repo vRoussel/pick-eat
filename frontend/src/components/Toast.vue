ff57df4bc7bfe97fb35d8f0efcb926a328497ff6
Valentin Roussel <valentin.rssl@gmail.com>
2024-03-08 07:16:14 +0100

    fix toast message appearance on small devices

--- frontend/src/components/Toast.vue
+++ frontend/src/components/Toast.vue
@@ -8,14 +8,10 @@ const props = defineProps({
 <template>
     <div class="toast">
         <div v-for="msg in props.error_queue" :key="msg" class="alert alert-error">
-            <div>
-                <span>{{ msg }}</span>
-            </div>
+            <p class="whitespace-normal">{{ msg }}</p>
         </div>
         <div v-for="msg in props.info_queue" :key="msg" class="alert alert-info">
-            <div>
-                <span>{{ msg }}</span>
-            </div>
+            <p class="whitespace-normal">{{ msg }}</p>
         </div>
     </div>
 </template>
