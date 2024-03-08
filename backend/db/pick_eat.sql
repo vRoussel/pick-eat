f088c77659bdb189b25a6f00751a1d0c512a6068
Valentin Roussel <valentin.rssl@gmail.com>
2024-03-08 08:45:01 +0100

    reduce random sorting collisions by using bigger prime numbers

--- backend/db/pick_eat.sql
+++ backend/db/pick_eat.sql
@@ -522,9 +522,9 @@ CREATE FUNCTION public.get_weekly_seed ()
 declare
     ret integer;
     idx integer;
-    primes integer[] := ARRAY[7, 11, 13, 17, 19, 23, 29, 31, 37, 41];
+    primes integer[] := ARRAY[101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199];
 begin
-    idx = extract('week' from current_date + interval '7 days') % ARRAY_LENGTH(primes, 1);
+    idx = extract('week' from current_date) % ARRAY_LENGTH(primes, 1);
     return primes[idx];
 end;
 $$;
